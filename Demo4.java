c7dc447a2ea835a633478be284dc731391cdd199
niti-di1977 <60072970+niti-di1977@users.noreply.github.com>
2022-08-24 10:39:43 +0530

    Create Demo4.java

---
+++ Demo4.java
@@ -0,0 +1,10 @@
+class Hello
+{
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Git");
+        System.out.println("Welcome to Git");
+        System.out.println("Welcome to Git");
+        System.out.println("Welcome to Git");
+    }
+}
